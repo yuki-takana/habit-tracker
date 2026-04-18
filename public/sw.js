c2af835a4a6c47699f8d5526b65df24d6644efb7
Abhishek Maurya <mauryaabhishek1508@gmail.com>
2026-04-18 23:35:14 +0530

    updated firebase secret

--- public/sw.js
+++ public/sw.js
@@ -1,32 +1,12 @@
-// self.addEventListener("push", function (event) {
-//   if (!event.data) return;
+self.addEventListener("push", function (event) {
+  if (!event.data) return;
 
-//   const data = event.data.json();
+  const data = event.data.json();
 
-//   self.registration.showNotification(data.title, {
-//     body: data.body,
-//     icon: "/icon.png",
-//     badge: "/icon.png",
-//     vibrate: [200, 100, 200],
-//   });
-// });
-importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
-
-firebase.initializeApp({
-  apiKey: "AIzaSyCRxir01RkeSWVA6GMJvVzkU1gVAiEfnF4",
-  authDomain: "todo-live-8ab83.firebaseapp.com",
-  projectId: "todo-live-8ab83",
-  messagingSenderId: "30547534734",
-  appId: "1:30547534734:web:6689f6b42a28badb097adf",
-});
-
-const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage((payload) => {
-  self.registration.showNotification(payload.notification.title, {
-    body: payload.notification.body,
-    icon: "/UFLLogo.png", 
-    badge: "/UFLLogo.png",
+  self.registration.showNotification(data.title, {
+    body: data.body,
+    icon: "/icon.png",
+    badge: "/icon.png",
+    vibrate: [200, 100, 200],
   });
 });
